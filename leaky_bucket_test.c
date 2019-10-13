648756e97aac88b365c4e83269a2e8be6f1aacbb
terriljoel <terriljoel@gmail.com>
2019-10-13 18:02:15 +0530

    nthng

--- leaky_bucket_test.c
+++ leaky_bucket_test.c
@@ -14,8 +14,8 @@ void timer(int);
 
 void init()
 {
-glClearColor(0.0,0.0,0.0,1.0);
-glMatrixMode(GL_PROJECTION);
+   glClearColor(0.0,0.0,0.0,1.0);
+   glMatrixMode(GL_PROJECTION);
    glLoadIdentity();
    gluOrtho2D(-500,500,-500,500);
    glMatrixMode(GL_MODELVIEW);
@@ -27,108 +27,108 @@ int status[100];
 int n,j,m;
 
 
- float y_pos[100],y_pos1[100],x_pos[100],angle[100];
+float y_pos[100],y_pos1[100],x_pos[100],angle[100];
 void draw(int x,int y)
-{
-for(j=0;j<n;j++){
-if(y_pos[j]>-510){
-glTranslatef(0,y_pos[j],0);
-glColor3f(1,1,1);
-glBegin(GL_POLYGON);
-
-    glVertex2f(-50,500);
-	glVertex2f(-50,400);
-	glVertex2f(50,400);
-	glVertex2f(50,500);
-glEnd();
-glLoadIdentity();
-}
-else if(status[j]==1)
-{
-glTranslatef(x_pos[j],y_pos[j],0);
-glRotatef(angle[j],0.0f,0.0f,1.0f);
-glColor3f(1,0,0);
-glBegin(GL_POLYGON);
-
-    glVertex2f(-50,500);
-	glVertex2f(-50,400);
-	glVertex2f(50,400);
-	glVertex2f(50,500);
-glEnd();
-glLoadIdentity();
-   glClearColor(0,0,0,1);
-}
-}
-glColor3f(1,1,1);
-glBegin(GL_POLYGON);
-
-    glVertex2f(-170,100);
-	glVertex2f(-100,-100);
-	glVertex2f(100,-100	);
-	glVertex2f(170,100);
-glEnd();
-   for(j=0;j<m;j++){
-glTranslatef(0,y_pos1[j],0);
-glColor3f(1,1,1);
- glBegin(GL_POLYGON);
-    glVertex2f(-25,25);
-	glVertex2f(-25,-25);
-	glVertex2f(25,-25);
-	glVertex2f(25,25);
-glEnd();
-glLoadIdentity();
-}
-
-}
+ {
+  for(j=0;j<n;j++)
+   {
+    if(y_pos[j]>-510)
+     {
+       glTranslatef(0,y_pos[j],0);
+       glColor3f(1,1,1);
+       glBegin(GL_POLYGON);
+       glVertex2f(-50,500);
+       glVertex2f(-50,400);
+       glVertex2f(50,400);
+       glVertex2f(50,500);
+       glEnd();
+       glLoadIdentity();
+     }
+    else if(status[j]==1)
+     {
+       glTranslatef(x_pos[j],y_pos[j],0);
+       glRotatef(angle[j],0.0f,0.0f,1.0f);
+       glColor3f(1,0,0);
+       glBegin(GL_POLYGON);
+       glVertex2f(-50,500);
+       glVertex2f(-50,400);
+       glVertex2f(50,400);
+       glVertex2f(50,500);
+       glEnd();
+       glLoadIdentity();
+       glClearColor(0,0,0,1);
+     }
+    }
+   glColor3f(1,1,1);
+   glBegin(GL_POLYGON);
+   glVertex2f(-170,100);
+   glVertex2f(-100,-100);
+   glVertex2f(100,-100	);
+   glVertex2f(170,100);
+   glEnd();
+   for(j=0;j<m;j++)
+    {
+     glTranslatef(0,y_pos1[j],0);
+     glColor3f(1,1,1);
+     glBegin(GL_POLYGON);
+     glVertex2f(-25,25);
+     glVertex2f(-25,-25);
+     glVertex2f(25,-25);
+     glVertex2f(25,25);
+     glEnd();
+     glLoadIdentity();
+    }
+ }
 
 void display()
-  {
-     glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
-     draw(50,50);
+ {
+   glClear(GL_COLOR_BUFFER_BIT);
+   glLoadIdentity();
+   draw(50,50);
 //printf("%f\t%f",y_pos[1],angle[1]);
 //glutSwapBuffers();
 //printf("%d",m);
-
-glFlush();
-}
+   glFlush();
+ }
 
 int k=1,l,f=0;
 void timer(int x)
-{
-   glutPostRedisplay();
-  glutTimerFunc(1000/5,timer,0);
-
-for(j=0;j<=k;j++)
-{
-if(y_pos[j]<-510 && status[j]==1){
-  x_pos[j]-=25;
-  angle[j]+=10; }
- 
-   if(j>0&&abs(y_pos[j-1]-y_pos[j])<150)
-   continue;
-  if(f==0){
-         y_pos[j]-=15;
-         f=1;
-         }
-   else
+ {
+    glutPostRedisplay();
+    glutTimerFunc(1000/5,timer,0);
+    for(j=0;j<=k;j++)
+     {
+      if(y_pos[j]<-450 && status[j]==1)
+       {
+        x_pos[j]-=25;
+        angle[j]+=10;
+       }
+      if(j>0&&abs(y_pos[j-1]-y_pos[j])<150)
+        continue;
+      if(f==0)
+       {
+        y_pos[j]-=15;
+        f=1;
+       }
+      else
+       {
+        y_pos[j]-=50;
+        f=0;
+       } 
+     }
+   if(y_pos[0]<-500)
+    { 
+     for(j=0;j<m;j++)
       {
-       y_pos[j]-=50;
-       f=0;
-      } 
-    }
-if(y_pos[0]<-500){
-for(j=0;j<m;j++)
-{
   //if(y_pos[j]>-505){
-   if(j>0&&abs(y_pos1[j-1]-y_pos1[j])<70)
-   continue;
-
-         y_pos1[j]-=10;
-   }}
-if(k<n)
-  k++;
-}
+       if(j>0&&abs(y_pos1[j-1]-y_pos1[j])<70)
+         continue;
+       y_pos1[j]-=10;
+      }
+     }
+   if(k<n)
+      k++;
+ }
 
 
 
@@ -215,6 +215,7 @@ void lky_bckt()
                      printf("\nAt time=%d: %d KB are transferred",i++,rem);
                       printf("\nRemaining bucket size is %d",excess);
                       rem=0;
+                      m++;
 
                 }
 
@@ -227,21 +228,19 @@ int main(int argc,char **argv)
     printf("Enter the number of packets--");
     scanf("%d",&n);
     input(n);
-
-
     lky_bckt();
 
-glutInit(&argc,argv);
-  glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
- glutInitWindowSize(500,500);
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_RGB|GLUT_SINGLE);
+    glutInitWindowSize(500,500);
 //status[1]=1;
 //status[2]=1;
-  glutInitWindowPosition(0,0);
-  glutCreateWindow("Translation");
-  glutDisplayFunc(display);
+    glutInitWindowPosition(0,0);
+    glutCreateWindow("Translation");
+    glutDisplayFunc(display);
   //glutReshapeFunc(reshape);
-  glutTimerFunc(0,timer,0);
-  init();
-  glutMainLoop();
+    glutTimerFunc(0,timer,0);
+   init();
+    glutMainLoop();
     return 0;
 }
